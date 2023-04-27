79940e431ec65857f26534da6dde032d6b595916
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2023-04-27 02:31:01 +0000

    Adicionado novo teste unitário relacionado ao login

--- src/profiles/tests/test_login.py
+++ src/profiles/tests/test_login.py
@@ -10,16 +10,15 @@ from rest_framework import status
 class LoginTestCase(APITestCase):
     def setUp(self) -> None:
         create_dummy_user()
-    
+        self.url = reverse('login')
+
     def test_login(self):
         '''
         Simula um usuário fazendo login no iDopt.
         '''
-        
+
         user = IdoptUser.objects.first()
-        
-        url = reverse('login')
-        
+
         payload = {
             'username': user.username,
             'password': '12345678',
@@ -27,19 +26,39 @@ class LoginTestCase(APITestCase):
                 'name': 'Galaxy S23',
                 'os': 'Android',
                 'os_version': '13',
-                
+
                 # FCM = Firebase Cloud Messaging. Usado para enviar notificações por PUSH.
                 'fcm_token': 'Token falso ahahahahahahah'
             }
         }
-        
-        response = self.client.post(url, payload, format='json')
+
+        response = self.client.post(self.url, payload, format='json')
         self.assertEqual(
-            response.status_code, 
-            status.HTTP_200_OK, 
+            response.status_code,
+            status.HTTP_200_OK,
             msg=json.dumps(response.data, indent=4)
         )
-        
+
         self.assertIsInstance(response.data['token'], str)
-        self.assertIsInstance(response.data['user'], dict, msg=json.dumps(response.data['user'], indent=4))
-        self.assertIsInstance(response.data['device'], dict, msg=json.dumps(response.data['device'], indent=4))
+        self.assertIsInstance(response.data['user'], dict, msg=json.dumps(
+            response.data['user'], indent=4))
+        self.assertIsInstance(response.data['device'], dict, msg=json.dumps(
+            response.data['device'], indent=4))
+
+    def test_invalid_password(self):
+        payload = {
+            'username': IdoptUser.objects.first().username,
+            'password': 'Invalid password',
+            'device': {
+                'name': 'Galaxy S23',
+                'os': 'Android',
+                'os_version': '13',
+
+                # FCM = Firebase Cloud Messaging. Usado para enviar notificações por PUSH.
+                'fcm_token': 'Token falso ahahahahahahah'
+            }
+        }
+
+        response = self.client.post(self.url, payload, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, json.dumps(
+            response.data, indent=4))
