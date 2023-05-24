966024b8acd4e35eb9efa2b88413880374b52055
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2023-05-24 14:34:51 +0000

    Finalizado CRUD de posts + testes
    Co-authored-by: NataliaBachiega <nataliabachiega@hotmail.com>
        Co-authored-by: Pietro-MM <morallespietro@gmail.com>
        Co-authored-by: ryymurata<ryy.murata@gmail.com>
        Co-authored-by: gabscastilho <gabrielly_castilho@outlook.com>
        Co-authored-by: CarolinaMartinsEmilio <carolinamartinsemilio@estudante.ufscar.br>

--- src/posts/api/urls.py
+++ src/posts/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from posts.api.views import ListCreatePostView
+from posts.api.views import ListCreatePostView, RetrieveDeletePostView
 
 urlpatterns = [
-    path('posts', ListCreatePostView.as_view(), name='posts'),
+    path('posts/', ListCreatePostView.as_view(), name='posts'),
+    path('posts/<int:pk>/', RetrieveDeletePostView.as_view(), name='post-detail')
 ]
