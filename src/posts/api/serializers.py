7297de80255bb74a29f12256742c22afd1d58bc0
gabscastilho <gabrielly_casitlho@outlook.com>
2023-05-26 20:04:30 +0000

    Adicionado serializador de comentários
    Co-authored-by: NataliaBachiega <nataliabachiega@hotmail.com>
    Co-authored-by: Lvght <viniciusquaresma1904@gmail.com>

--- src/posts/api/serializers.py
+++ src/posts/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from posts.models import Post
+from posts.models import Post, Comment
 from profiles.api.serializers import UserSerializer
 
 
@@ -10,3 +10,14 @@ class PostSerializer(serializers.ModelSerializer):
         fields = '__all__'
     
     author = UserSerializer(required=False)
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Comment
+        fields = '__all__'
+
+    answer_of = serializers.PrimaryKeyRelatedField(
+        queryset = Comment.objects.all(),
+        required = False,
+        allow_null = True,
+    )
